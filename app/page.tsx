2995ec5097ec721d1e432543e009b38c38585dc5
zurgl <zurgly@gmail.com>
2023-08-31 17:33:46 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex justify-center items-center text-4xl">
-        My Memory on a bad journey
+        My Memory regarding a bad journey
       </div>
     </main>
   )
