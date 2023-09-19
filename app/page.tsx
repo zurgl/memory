e96bc4027c8c3ac3311d58270f1d49d725b41f6b
zurgl <zurgly@gmail.com>
2023-09-19 17:46:50 +0200

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex justify-center items-center text-4xl">
-        My Memory regarding a bad journey
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-8">
+      <div className="text-4xl">From sketch to art, using generative AI</div>
+      <div className="xs:flex-col lg:flex justify-center gap-4 font-black">
+        <Image
+          src="/ezty.png"
+          width={300}
+          height={300}
+          alt="Picture of the author"
+        />
+        <Image
+          src="/eztia01.png"
+          width={300}
+          height={300}
+          alt="Picture of the author"
+        />
+        <Image
+          src="/eztia02.png"
+          width={300}
+          height={300}
+          alt="Picture of the author"
+        />
       </div>
     </main>
-  )
+  );
 }
