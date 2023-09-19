32573f96d1648b1b99c3ae2f6110ce4d88afbe70
zurgl <zurgly@gmail.com>
2023-09-19 17:49:58 +0200

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-8">
-      <div className="text-4xl">From sketch to art</div>
-      <div className="text-4xl">Using generative AI</div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-4">
+      <div className="text-4xl font-black">From sketch to art</div>
+      <div className="text-xl">Using generative AI</div>
       <div className="xs:flex-col lg:flex justify-center gap-4 font-black">
         <Image
           src="/eztia01.png"
